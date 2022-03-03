dbadbabe9debaec3f197733b08e444adf62a7d50
DESKTOP-ECVK5RE\weitz <weitze97@gapp.nthu.edu.tw>
2022-03-04 03:51:36 +0800

    preprocess on webpage

--- weichih/taco2/app2.py
+++ weichih/taco2/app2.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask import render_template
+from preprocess_v1_1_function import preprocess
+
 app = Flask(__name__)     # 建立Application物件
 
 # 建立網站首頁的回應方式
-@app.route("/")  # 創造出網域下名為"/"的網址
+@app.route("/home")  # 創造出網域下名為"/"的網址
 def home():    # view函式
     return render_template("home.html")   # 回傳網站首頁內容
+
+@app.route("/home/pred")
+def pred():
+    preprocess()
+    return render_template("pred.html")
 
 app.run() ##
